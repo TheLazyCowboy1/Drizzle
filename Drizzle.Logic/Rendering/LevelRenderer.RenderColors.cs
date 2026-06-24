13c6cfba965abaaabdc14fb646869ecfbdd08a9b
TheLazyCowboy1 <Nathan@331oak.com>
2026-06-24 19:53:19 -0400

    crazy attempt for full render

--- Drizzle.Logic/Rendering/LevelRenderer.RenderColors.cs
+++ Drizzle.Logic/Rendering/LevelRenderer.RenderColors.cs
@@ -37,7 +37,7 @@ public sealed partial class LevelRenderer
         var bkgFix = (LingoNumber)Movie.bkgFix > 0;
 
         var c = (int)Movie.c - 1;
-        for (var q = 0; q < 1400; q++)
+        for (var q = 0; q < 2000; q++)
         {
             // NOTE: q and c are shifted by one compared to the original Lingo code.
             // This is much more sane, but keep it in mind.
@@ -237,7 +237,7 @@ public sealed partial class LevelRenderer
 
         Movie.c += 1;
 
-        if (Movie.c > 800)
+        if (Movie.c > 1200)
         {
             Movie.c += 1;
             Movie.keepLooping = (LingoNumber)0;
